cd96c90cf4e2625a2b807d1ab7c0a393d8e94c6a
Fabian Raetz <fabian.raetz@gmail.com>
2018-10-17 14:21:42 +0200

    redeploy crowdsale-dapp with correct homepage settings

---
+++ docs/build/precache-manifest.9c041c1d0451f88a06196e71b74ebcd8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ccaccbcd9299dadfb091",
+    "url": "/crowdsale/static/js/runtime~main.ccaccbcd.js"
+  },
+  {
+    "revision": "f986eb52065b6638511c",
+    "url": "/crowdsale/static/js/main.f986eb52.chunk.js"
+  },
+  {
+    "revision": "05486c1b9bb9d3397b11",
+    "url": "/crowdsale/static/js/1.05486c1b.chunk.js"
+  },
+  {
+    "revision": "f986eb52065b6638511c",
+    "url": "/crowdsale/static/css/main.0dfb7a0b.chunk.css"
+  },
+  {
+    "revision": "7755d7ae7056635fc8148f2905b49aa3",
+    "url": "/crowdsale/index.html"
+  }
+];
---
+++ docs/build/service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+
+importScripts(
+  "/crowdsale/precache-manifest.9c041c1d0451f88a06196e71b74ebcd8.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/crowdsale/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
